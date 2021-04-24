90c3f2d1f516d05807bfc1a05e6b02d5c9cb047c
esynr3z <esynr3z@gmail.com>
2021-04-24 10:40:06 +0300

    Fix simulation scripts

--- sim/elab.sh
+++ sim/elab.sh
@@ -2,11 +2,11 @@
 
 # prepare elaboration args
 cat <<EOT > files.prj
-verilog work ../../src/axi_ram/axi_ram.v
-sv work ../../src/axi_vip_master/axi_vip_master_pkg.sv
-sv work ../../src/axi_vip_master/axi_vip_v1_1_vl_rfs.sv
-sv work ../../src/axi_vip_master/axi_vip_master.sv
-sv work ../../src/tb/tb.sv
+verilog work ../src/axi_ram/axi_ram.v
+sv work ../src/axi_vip_master/axi_vip_master_pkg.sv
+sv work ../src/axi_vip_master/axi_vip_v1_1_vl_rfs.sv
+sv work ../src/axi_vip_master/axi_vip_master.sv
+sv work ../src/tb/tb.sv
 EOT
 XELAB_ARGS="--debug typical --incr --relax --prj files.prj work.tb -L xilinx_vip"
 
--- sim/elab_nolib.sh
+++ sim/elab_nolib.sh
@@ -2,17 +2,17 @@
 
 # prepare elaboration args
 cat <<EOT > files.prj
-verilog work ../../src/axi_ram/axi_ram.v
-sv work ../../src/xilinx_vip/xil_common_vip_pkg.sv
-sv work ../../src/xilinx_vip/axi_vip_pkg.sv
-sv work ../../src/xilinx_vip/axi_vip_axi4pc.sv
-sv work ../../src/xilinx_vip/axi_vip_if.sv
-sv work ../../src/axi_vip_master/axi_vip_master_pkg.sv
-sv work ../../src/axi_vip_master/axi_vip_v1_1_vl_rfs.sv
-sv work ../../src/axi_vip_master/axi_vip_master.sv
-sv work ../../src/tb/tb.sv
+verilog work ../src/axi_ram/axi_ram.v
+sv work ../src/xilinx_vip/xil_common_vip_pkg.sv
+sv work ../src/xilinx_vip/axi_vip_pkg.sv
+sv work ../src/xilinx_vip/axi_vip_axi4pc.sv
+sv work ../src/xilinx_vip/axi_vip_if.sv
+sv work ../src/axi_vip_master/axi_vip_master_pkg.sv
+sv work ../src/axi_vip_master/axi_vip_v1_1_vl_rfs.sv
+sv work ../src/axi_vip_master/axi_vip_master.sv
+sv work ../src/tb/tb.sv
 EOT
-XELAB_ARGS="--debug typical --incr --relax --prj files.prj work.tb -i ../../src/xilinx_vip"
+XELAB_ARGS="--debug typical --incr --relax --prj files.prj work.tb -i ../src/xilinx_vip"
 
 # do elaboration
 xelab ${XELAB_ARGS}
